99dc2df6a80803a3dbaa7e009590f5c27ee6615d
tarry87 <46358346+tarry87@users.noreply.github.com>
2020-02-07 13:30:56 +0530

    updated the chrome driver location
    updated the chrome driver location

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -44,7 +44,7 @@ public class Hook extends BaseUtil{
 //        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
 //        base.Driver = new ChromeDriver();
 
-        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
         base.Driver = new ChromeDriver();
     }
 
